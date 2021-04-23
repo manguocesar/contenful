8f7098a6c5c3ee7f8735a12608632b7b2877a0c5
cesarcoderbunker <cesar.hertz@coderbunker.com>
2021-04-23 23:19:23 +0800

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,42 @@
-export default function Recipes() {
+
+import { createClient } from 'contentful'
+
+import RecipeCard from './RecipeCard'
+
+export async function getStaticProps(){
+
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  })
+
+  const res = await client.getEntries({
+    content_type: 'recipe'
+  })
+  return {
+    props: {
+      recipes: res.items
+    }
+  }
+
+}
+
+export default function Recipes({recipes}) {
+  console.log('recipes',recipes);
   return (
     <div className="recipe-list">
-      Recipe List
+      {recipes.map(recipe =>(
+       <RecipeCard key={recipe.sys.id} recipe={recipe} />
+      ))}
+
+<style jsx>{`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 20px 60px;
+        }
+      `}</style>
+    
     </div>
   )
 }
