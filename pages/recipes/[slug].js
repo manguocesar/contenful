6d36c2975318ade965a0b46da54fecd663fca752
cesarcoderbunker <cesar.hertz@coderbunker.com>
2021-04-24 22:42:20 +0800

    fallback

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -2,6 +2,8 @@ import { createClient } from 'contentful'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import Image from 'next/image'
 
+import Skeleton from '../../components/Skeleton'
+
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_KEY,
@@ -20,7 +22,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false
+    fallback: true
   }
 }
 
@@ -38,6 +40,8 @@ export const getStaticProps = async ({ params }) => {
 }
 
 export default function RecipeDetails({ recipe }) {
+  if (!recipe) return <Skeleton/>
+
   const { featuredImage, title, cookingTime, ingredients, method } = recipe.fields
   console.log(method)
 
