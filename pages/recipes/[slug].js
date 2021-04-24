d24e1de8f7e7a990f048ecd86d34ea977f1b0ad2
cesarcoderbunker <cesar.hertz@coderbunker.com>
2021-04-24 16:46:33 +0800

    now!

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,43 +1,45 @@
 import { createClient } from 'contentful'
-import Image from 'next/image'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import Image from 'next/image'
 
-  const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+})
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({ 
+    content_type: "recipe" 
   })
 
-  export async function getStaticPaths(){
-
-  const res = await client.getEntries({
-    content_type: 'recipe'
-  })
-  const paths = res.items.map(item=>{
+  const paths = res.items.map(item => {
     return {
-      params: {slug: item.fields.slug}
+      params: { slug: item.fields.slug }
     }
-
-    
   })
-  return {paths:paths, fallback: false}
 
+  return {
+    paths,
+    fallback: false
+  }
 }
 
-export async function getStaticProps({params}){
-  const {items} = await client.getEntries({
+export const getStaticProps = async ({ params }) => {
+  const { items } = await client.getEntries({
     content_type: 'recipe',
-    'fields.slug' : params.slug
-  }) 
+    'fields.slug': params.slug
+  })
+
   return {
-    props: { recipe: items[0]}
+    props: { recipe: items[0] }
   }
 
 }
 
-export default function RecipeDetails({recipe}) {
- 
-  const {featuredImage, title, cookingTime, ingredients, method} = recipe.fields
- 
+export default function RecipeDetails({ recipe }) {
+  const { featuredImage, title, cookingTime, ingredients, method } = recipe.fields
+  console.log(method)
+
   return (
     <div>
       <div className="banner">
